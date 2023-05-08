869845789d9a8fe2a2d84f0dfdb51c56f494bac3
LEO <leo.janela@gmail.com>
2023-05-08 14:39:51 -0300

    Third Commit - Database feita usando SQLite3, Database padrao do django

--- projetoApp/admin.py
+++ projetoApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from projetoApp.models import ToDoItem, ToDoList
+
+#Importa as classes vindas das implementacoes de cada item da database criado no models.py
+admin.site.register(ToDoItem)
+admin.site.register(ToDoList)
