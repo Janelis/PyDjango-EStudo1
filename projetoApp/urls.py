2de9bdd3aca53a7d66aff968300e43ffa1269fdc
LEO <leo.janela@gmail.com>
2023-05-09 10:26:20 -0300

    Pagina todo_list.html adicionada, urls.py e views.py ajustados para suprirem metodos e caminhos necessarios

--- projetoApp/urls.py
+++ projetoApp/urls.py
@@ -1,6 +1,12 @@
-#/PyDjantoEStudo1/projetoApp/urls.py
+#/PyDjangoEStudo1/projetoApp/urls.py
 
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    #Se a URL estiver  vazia, "", chamar a classe ModelListView para resolver a request, mandando para a pagina "index"
+    path("", views.ModelListView.as_view(), name="index"),
+    
+    #se a URL corresponder a lista de numero X, executa o metodo as_view() da classe ItemListView para o objeto correspondente.
+    path("list/<int:list_id>/", views.ItemListView.as_view(), name="list"),
 ]
--- projetoApp/views.py
+++ projetoApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.views.generic import ListView
-from .models import ToDoList
+from .models import ToDoList, ToDoItem
 
 
 #Classe responsavel por as listas de afazeres na forma de lista. Funcao ja sabe puxar uma lista de objetos da db pelo ListView importado anteriormente
@@ -11,3 +11,17 @@ class ModelListView(ListView):
     model = ToDoList
     template_name = "projetoApp/index.html"
 
+#Classe responsavel por mostrar items dentro de uma lista na forma de lista.
+class ItemListView(ListView):
+    model = ToDoItem
+    template_name = "todo_app/todo_list.html"
+
+    #Redefine a funcao get_queryset para usar os objetos filtrados 
+    def get_queryset(self):
+        return ToDoItem.objects.filter(todo_list_id=self.kwargs["list_id"])
+
+    #Redefine a funcao get_context_data para retornar os objetos correspondentes ao filtro dentro de .get()
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["todo_list"] = ToDoList.objects.get(id=self.kwargs["list_id"])
+        return context
