be02dbbfa4917bbb64210deb5675d96c60356353
LEO <leo.janela@gmail.com>
2023-05-09 09:47:06 -0300

    criacao de views

--- projetoApp/views.py
+++ projetoApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import ListView
+from .models import ToDoList
+
+
+#Classe responsavel por as listas de afazeres na forma de lista. Funcao ja sabe puxar uma lista de objetos da db pelo ListView importado anteriormente
+class ModelListView(ListView):
+    model = ToDoList
+    template_name = "projetoApp/index.html"
+
